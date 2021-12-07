a4bf0cc73db06bd43446150cfbee4cf991717418
Dernan1 <95646856+Dernan1@users.noreply.github.com>
2021-12-07 22:11:02 +0000

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -62,39 +62,32 @@ compute() {
 
    }
 
-getDisplayNumber(number) {
-	const stringNumber = number.toString()
-	const integerDigits = parseFloat(stringNumber.split('.')[0])
-	const decimalDigits = stringNumber.split('.')[1]
-	let integerDisplay
-	if (isNaN(integerDigits)) {
-     integerDisplay = ''
-  } else {
-  	 integerDisplay = integerDigits.toLocaleString('en', {
-  	 maximumFractionDigits: 0 })
-  }
-    if (decimalDigits != null) {return `${integerDisplay}.${decimalDigits}`
-    
-  } else {
+  getDisplayNumber(number) {
+    const stringNumber = number.toString()
+    const integerDigits = parseFloat(stringNumber.split('.')[0])
+ 	  const decimalDigits = stringNumber.split('.')[1]
+	  let integerDisplay
+	  if (isNaN(integerDigits)) {
+      integerDisplay = ''
+    } else {
+  	  integerDisplay = integerDigits.toLocaleString('en', {
+  	  maximumFractionDigits: 0 })
+    }
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`
+    } 
     return integerDisplay
+  }
+ 
+  updateDisplay() {
+    this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
+    if (this.operation != null) {
+      this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+    } else {
+  	  this.previousOperandTextElement.innerText = ''
     }
   }
-
-  	  /*const floatNumber = parseFloat(number)
-    if (isNaN(floatNumber)) return ''
-  	return floatNumber.tolocaleString('en')*/
-   
-  
-
-updateDisplay() {
-	this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
-	if (this.operation != null) {
-    this.previousOperandTextElement.innerText = `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
-  } else {
-  	this.previousOperandTextElement.innerText = ''
-      }
-    }
-  }
+}
 
 
 
